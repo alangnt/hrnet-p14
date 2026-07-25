961c550630316b6d0b32aae88efb8eda0d7dd3ff
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-07-25 12:31:36 +0200

    feat: added ids on the inputs

--- src/components/EmployeeForm.tsx
+++ src/components/EmployeeForm.tsx
@@ -14,39 +14,39 @@ function EmployeeForm({ onSubmit }: EmployeeFormProps) {
       }}
     >
       <label htmlFor="first-name">First Name</label>
-      <input type="text" name={"firstName"}/>
+      <input type="text" id="first-name" name={"firstName"}/>
 
       <label htmlFor="last-name">Last Name</label>
-      <input type="text" name={"lastName"}/>
+      <input type="text" id="last-name" name={"lastName"}/>
 
       <label htmlFor="date-of-birth">Date of Birth</label>
-      <input type="date" name={"dateOfBirth"}/>
+      <input type="date" id="date-of-birth" name={"dateOfBirth"}/>
 
       <label htmlFor="start-date">Start Date</label>
-      <input type="date" name={"startDate"}/>
+      <input type="date" id="start-date" name={"startDate"}/>
 
       <fieldset className="address">
         <legend>Address</legend>
 
         <label htmlFor="street">Street</label>
-        <input type="text" name={"street"}/>
+        <input type="text" id="street" name={"street"}/>
 
         <label htmlFor="city">City</label>
-        <input type="text" name={"city"}/>
+        <input type="text" id="city" name={"city"}/>
 
         <label htmlFor="state">State</label>
-        <select name="state">
+        <select id="state" name="state">
           {states.map((state) => (
             <option key={state.abbreviation} value={state.abbreviation}>{state.name}</option>
           ))}
         </select>
 
         <label htmlFor="zip-code">Zip Code</label>
-        <input type="number" name={"zipCode"}/>
+        <input type="number" id="zip-code" name={"zipCode"}/>
       </fieldset>
 
       <label htmlFor="department">Department</label>
-      <select name="department" id="department">
+      <select id="department" name="department">
         <option>Sales</option>
         <option>Marketing</option>
         <option>Engineering</option>
