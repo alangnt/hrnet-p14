b3bb167c1ef4c655721a21a858e4122c8ee05c77
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-07-25 13:19:29 +0200

    feat: better handling of the table

--- src/pages/EmployeeList.tsx
+++ src/pages/EmployeeList.tsx
@@ -1,56 +1,57 @@
 import '../App.css'
+import { useMemo } from "react";
 import { Link } from "react-router";
 import type { User } from "../types/users";
 
 type Column = {
   title: string;
-  data: string;
+  data: keyof User;
 }
 
-function EmployeeList() {
-  const stored: User[] = JSON.parse(localStorage.getItem('employees') as string) || [];
-  const employees: User[] = stored.map((employee, index) => (
-    employee.id ? employee : { ...employee, id: `legacy-${index}` }
-  ));
+const columns: Column[] = [
+  { title: 'First Name', data: 'firstName' },
+  { title: 'Last Name', data: 'lastName' },
+  { title: 'Start Date', data: 'startDate' },
+  { title: 'Department', data: 'department' },
+  { title: 'Date of Birth', data: 'dateOfBirth' },
+  { title: 'Street', data: 'street' },
+  { title: 'City', data: 'city' },
+  { title: 'State', data: 'state' },
+  { title: 'Zip Code', data: 'zipCode' },
+];
 
-  const columns: Column[] = [
-    { title: 'First Name', data: 'firstName' },
-    { title: 'Last Name', data: 'lastName' },
-    { title: 'Start Date', data: 'startDate' },
-    { title: 'Department', data: 'department' },
-    { title: 'Date of Birth', data: 'dateOfBirth' },
-    { title: 'Street', data: 'street' },
-    { title: 'City', data: 'city' },
-    { title: 'State', data: 'state' },
-    { title: 'Zip Code', data: 'zipCode' },
-  ];
+function EmployeeList() {
+  const employees = useMemo<User[]>(() => {
+    const stored: User[] = JSON.parse(localStorage.getItem('employees') as string) || [];
+    return stored.map((employee, index) => (
+      employee.id ? employee : { ...employee, id: `legacy-${index}` }
+    ));
+  }, []);
 
   return (
     <main id="employee-div" className="container">
       <h1>Current Employees</h1>
+
       <table id="employee-table" className="display">
-        <tbody>
+        <thead>
           <tr>
             {columns.map((column: Column) => (
-              <th key={column.data}>{column.title}</th>
+              <th key={column.data} scope="col">{column.title}</th>
             ))}
           </tr>
+        </thead>
 
+        <tbody>
           {employees.map((employee: User) => (
             <tr key={employee.id}>
-              <td>{employee.firstName}</td>
-              <td>{employee.lastName}</td>
-              <td>{employee.startDate}</td>
-              <td>{employee.department}</td>
-              <td>{employee.dateOfBirth}</td>
-              <td>{employee.street}</td>
-              <td>{employee.city}</td>
-              <td>{employee.state}</td>
-              <td>{employee.zipCode}</td>
+              {columns.map((column: Column) => (
+                <td key={column.data}>{employee[column.data]}</td>
+              ))}
             </tr>
           ))}
         </tbody>
       </table>
+
       <Link to="/">Home</Link>
     </main>
   )
